449dac6e3187ccf49f0351d951431fb00dca4f6f
Martu Riesco <marturiesco.costa@hotmail.com>
2022-10-27 13:43:46 -0300

    desafio1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-let edad;
+/*let numero1 = prompt ("Ingrese un numero 1");
+let numero2 = prompt ("Ingrese un numero 2");
+alert(` la suma de sus numeros es ${parseInt(numero1)+ parseInt(numero2)}`);*/
+
+
+/*let pres = prompt ("ingrese su nombre")
+if (pres === "ana"); {
+ alert("Hola sos el amor de mi vida");
+}else{
+    alert("no sos el amor de mi vida")
+}*/
+ 
+/*let edad = parseInt(prompt("Ingrese su edad"));
+if(edad < 15){
+    alert("no podes entrar a la fiesta");
+}else if(edad <18){
+    alert("podes entrar con un mayor");
+}else{
+    alert("podes entrar");
+}*/
+/*let numero1 =Number( prompt("ingrese un numero 1"));
+let numero2 = Number(prompt("ingrese un numero 2"));
+if (numero1>numero2){
+    alert("el primer numero es mas grande");
+}else if(numero2>numero1){
+    alert("el segundo numero es mas grande");
+}else{
+    alert("sus numeros son iguales");
+}*/
+
+for(let i=1; i<=10; i++){
+    if(i=== 5){
+     console.log("entramos al break")
+      break;
+      }
+      console.log ("hola todos", i)
+ }
+ console.log ("proceso finalizado")
---
+++ principal.js
@@ -0,0 +1,11 @@
+let presentacion = alert("Bienvenidos a Wachas en Bombachas!")
+let malla = prompt ("seleccione el tipo de malla que desea comprar: 1. Bikini 2. Enteriza")
+if (malla==1){
+    alert("selecciono Bikini");
+}else if(malla==2){
+    alert("selecciono Enteriza");
+}else{
+    alert("Opcion no valida")
+}
+let tallle = prompt ("Seleccione el talle: .1 85/90 2.90/95 3.95/100 4.100/105")
+
