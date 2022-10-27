c46dfe082f52642725127808aaaf8f73254f07e5
Martu Riesco <marturiesco.costa@hotmail.com>
2022-10-27 14:27:30 -0300

    while

--- principal.js
+++ principal.js
@@ -7,5 +7,26 @@ if (malla==1){
 }else{
     alert("Opcion no valida")
 }
-let tallle = prompt ("Seleccione el talle: .1 85/90 2.90/95 3.95/100 4.100/105")
+let talle = prompt ("Seleccione el talle: a. 85/90 b.90/95 c.95/100 d.100/105 e.Otro");
 
+while(talle!="esc"){
+   switch (talle) {
+       case "a":
+            alert("contamos con 5 modelos de este talle");
+            break;
+        case "b":
+            alert("contamos con 2 modelos de este talle");
+            break;
+        case "c":
+            alert("contamos con 4 modelos de este talle");
+            break;
+        case "d":
+            alert("contamos con 3 modelos de este talle");
+            break;
+        case "e":
+            alert("contamos con 2 modelos de este talle");
+            break;
+       default:
+           alert("Opcion invalida")
+           break;
+   }}
