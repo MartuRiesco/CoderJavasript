d59f38ae9028c223ba22ad633dd030bd78ffb44a
Martu Riesco <marturiesco.costa@hotmail.com>
2022-11-02 12:33:19 -0300

    codigo

--- principal.js
+++ principal.js
@@ -4,14 +4,25 @@ let presentacion = alert("Bienvenidos a Wachas en Bombachas!")
 let malla;
 let flag;
 let talle;
+let mod;
+let encontrado;
+const productos = [
+    { id: 1, nombre: "janina", precio: 1000 },
+    { id: 2, nombre: "corallina", precio: 700 },
+    { id: 3, nombre: "volvox", precio: 800 },
+    { id: 4, nombre: "clytia", precio: 1750 },
+    {id: 5, nombre: "laminaria", precio: 2000},
+  ];
 
 mallas();
+encontrar();
 modelos();
  let das= true;
     do
     {let desea = prompt("Desea seguir comprando?");
     if (desea == "si") {
         mallas();
+        encontrar();
         modelos();
     } else {
         das= false;
@@ -28,13 +39,36 @@ while( (malla!="1" && malla!="2" )){
 
 if (malla == 1) {
     alert("selecciono Bikini");
-    document.write(" Bikini")
+    document.write(" Bikini ")
 } else if (malla == 2) {
     alert("selecciono Enteriza");
-    document.write(" Enteriza")
+    document.write(" Enteriza ")
 }
+}
+  /* seleccion de modelos */
+ function encontrar () {
+    alert("los modelos disponibles son: Janina, Corallina, Volvox, Clytia y Laminaria")
+      mod = prompt("Ingrese el nombre del producto que desea").toLowerCase();
+    encontrado = false;
+    while((mod!="janina" && mod!="corallina" && mod!="volvox" && mod!="clytia" && mod!="laminaria")){
+        mod = prompt("No contamos con ese modelo. Seleccione alguno de los modelos disponibles: Janina, Corallina, Volvox, Clytia y Laminaria")
+    }
+    
+    for(const producto of productos){
+      if(producto.nombre === mod){
+        alert('selecciono '+ mod)
+        document.write(mod)
+        encontrado = true;
+      }
+    }
+    if(encontrado){
+      alert("Producto disponible");
+    }else{
+      alert("Producto no disponible");
+    }
 }
 
+  
 /* seleccion de talle de malla */
  
 /* aviso de stock */
@@ -68,9 +102,13 @@ function modelos(){
                 alert("contamos con 2 modelos de este talle");
                 document.write(" talle 105/+")
                 flag = false;
-                break;   default:
+                break;  
+            default:
                 alert("Opcion invalida")
                 break;
         }
     }
+}
+function suma (){
+    let index = productos.indexOf(precio)
 }
